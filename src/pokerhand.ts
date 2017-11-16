74316fce050310f9176d14b8b25d112f6a53fbce
DIWithers <d.ivy.withers@gmail.com>
2017-11-16 09:32:20 -0500

    Got rid of all of the IF statements and plus equals with a direct reference to the map

--- src/pokerhand.ts
+++ src/pokerhand.ts
@@ -2,7 +2,7 @@ export class PokerHandRank {
 
     public static findHighestCard(cards: string): string {
         console.log("cards: " + cards);
-        let rank: string = "High Card: ";
+
         let cardRankWord: any =  {
             "A": "Ace",
             "8": "Eight",
@@ -12,15 +12,6 @@ export class PokerHandRank {
         let highestCard: any = this.sortCards(cards)[0][0];
         console.log(highestCard);
         return "High Card: " + cardRankWord[highestCard];
-        if (highestCard === "A") {
-            return rank += "Ace";
-        }
-        if (highestCard === "8") {
-            return rank += "Eight";
-        }
-        rank += cardRankWord[highestCard];
-        console.log();
-        return rank;
     }
 
     public static sortCards(cardsToSort: string): any {
