d1fa41d4532afe96d5b51b2a946c2eecd88f4f0d
DIWithers <d.ivy.withers@gmail.com>
2017-11-16 19:02:01 -0500

    find 3 of a kind

--- spec/pokerhand.spec.ts
+++ spec/pokerhand.spec.ts
@@ -2,15 +2,19 @@ import {PokerHandRank} from "../src/pokerhand";
 
 describe("function name", () => {
     it("should determine highest value card with Ace", () => {
-        expect(PokerHandRank.findHighestCard("3H 2C 4S AH 5C")).toBe("High Card: Ace");
+        expect(PokerHandRank.findRank("3H 2C 4S AH 5C")).toBe("High Card: Ace");
     });
     it("should determine highest value card with 8", () => {
-        expect(PokerHandRank.findHighestCard("2C 3H 4S 8C 5H")).toBe("High Card: Eight");
+        expect(PokerHandRank.findRank("2C 3H 4S 8C 5H")).toBe("High Card: Eight");
     });
     it("should determine highest value card with 9", () => {
-        expect(PokerHandRank.findHighestCard("2C 3H 4S 9C 5H")).toBe("High Card: Nine");
+        expect(PokerHandRank.findRank("2C 3H 4S 9C 5H")).toBe("High Card: Nine");
     });
     it("should determine if highest rank is pair(2 cards of same value)", () => {
-        expect(PokerHandRank.findRank("2C 3H 4S 9C 9H")).toBe("Pair");
+        expect(PokerHandRank.findRank("2C 4H 4S 3C 9H")).toBe("Pair");
     });
+    it("should determine if highest rank is 3 of a kind(3 cards of same value)", () => {
+        expect(PokerHandRank.findRank("2C 3H 3S 3C 9H")).toBe("Three of a Kind");
+    });
+
 });
--- src/pokerhand.ts
+++ src/pokerhand.ts
@@ -1,7 +1,7 @@
 export class PokerHandRank {
 
-    public static findHighestCard(cards: string): string {
-        console.log("cards: " + cards);
+    public static findHighestCard(sortedCards: any): string {
+        console.log("cards: " + sortedCards);
 
         let cardRankWord: any =  {
             "A": "Ace",
@@ -9,23 +9,48 @@ export class PokerHandRank {
             "9": "Nine"
         }
 
-        let highestCard: any = this.sortCardsHighestToLowest(cards)[0][0];
-        console.log(highestCard);
+        let highestCard: any = sortedCards[0][0];
+        // console.log(highestCard);
         return "High Card: " + cardRankWord[highestCard];
     }
 
     public static sortCardsHighestToLowest(cardsToSort: string): any {
-        console.log(cardsToSort.split(" ").sort().reverse());
+        // console.log(cardsToSort.split(" ").sort().reverse());
         return cardsToSort.split(" ").sort().reverse();
     }
     private static isPair(sortedCards: string): boolean {
-        console.log(sortedCards);
-        return sortedCards[0][0] === sortedCards[1][0];
+        let consecutiveCardCount: number = this.findNumberOfConsecutiveCards(sortedCards);
+        return consecutiveCardCount === 1;
+    }
+    private static isThreeOfAKind(sortedCards: string): boolean {
+        let consecutiveCardCount: number = this.findNumberOfConsecutiveCards(sortedCards);
+        return consecutiveCardCount === 2;
+    }
+
+    private static findNumberOfConsecutiveCards(sortedCards: string): number {
+        let lastCard: string = "";
+        let consecutiveCardCount: number = 0;
+        for (let i: number = 0; i < sortedCards.length; i++) {
+            console.log(lastCard[0]);
+            console.log(sortedCards[i][0]);
+            if (sortedCards[i][0] === lastCard[0]) {
+                consecutiveCardCount++;
+            }
+
+            lastCard = sortedCards[i];
+            console.log(consecutiveCardCount);
+
+        }
+        return consecutiveCardCount;
     }
     public static findRank(cards: any): string {
         let sortedCards: string = this.sortCardsHighestToLowest(cards);
+        if (this.isThreeOfAKind(sortedCards)) {
+            return "Three of a Kind";
+        }
         if (this.isPair(sortedCards)) {
             return "Pair";
         }
+        else return this.findHighestCard(sortedCards);
     }
 }
