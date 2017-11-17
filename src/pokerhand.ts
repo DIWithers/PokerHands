35b87dd5b23a55839c549c6cbd2a5ecbcd72cb6e
DIWithers <d.ivy.withers@gmail.com>
2017-11-17 16:27:43 -0500

    Find Pair

--- src/pokerhand.ts
+++ src/pokerhand.ts
@@ -1,15 +1,32 @@
 export class PokerHand {
 
     static findRank(cards: string): string {
-        return this.findHighestCard(cards);
+        let sortedCards: any = cards.split(" ").sort().reverse();
+        let consecutiveHops: number = 0;
+        let lastCard: string = "";
+        console.log(sortedCards.length);
+        for (let i: number = 0; i < sortedCards.length; i++) {
+            console.log(sortedCards[i][0] + " , " + lastCard[0]);
+
+            if (sortedCards[i][0] === lastCard[0]) {
+                consecutiveHops++;
+            }
+            lastCard = sortedCards[i];
+        }
+        console.log(consecutiveHops);
+        if (consecutiveHops === 1) {
+            return "Pair";
+        }
+
+        return this.findHighestCard(sortedCards);
     }
 
-    private static findHighestCard(cards: string): string {
+    private static findHighestCard(sortedCards: string): string {
         let cardWords: any = {
             "A": "Ace",
             "8": "Eight"
         }
-        let sortedCards: any = cards.split(" ").sort().reverse();
+
         let highestCardValue: string = cardWords[sortedCards[0][0]];
         return "High Card: " + highestCardValue;
     }
