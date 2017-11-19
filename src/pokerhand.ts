d6fed7695fda27c69a882605942d0b17bb1dffe4
DIWithers <d.ivy.withers@gmail.com>
2017-11-19 08:21:46 -0500

    STRATEGY CHANGE to finding highest card

--- src/pokerhand.ts
+++ src/pokerhand.ts
@@ -1,38 +1,42 @@
 export class PokerHand {
-
     static findRank(cards: string): string {
         let sortedCards: any = cards.split(" ").sort().reverse();
-        let consecutiveHops: number = this.findConsecutiveHops(sortedCards);
-        console.log(consecutiveHops);
-        if (consecutiveHops === 1) {
-            return "Pair";
-        }
-
-        return this.findHighestCard(sortedCards);
+        let cardInfo: any = PokerHand.updateOccurances(sortedCards);
+        console.log(cardInfo);
+        return this.findHighestCard(sortedCards, cardInfo);
     }
 
-    private static findConsecutiveHops(sortedCards: any) {
-        let consecutiveHops: number = 0;
-        let lastCard: string = "";
-        console.log(sortedCards.length);
-        for (let i: number = 0; i < sortedCards.length; i++) {
-            console.log(sortedCards[i][0] + " , " + lastCard[0]);
+    private static updateOccurances(sortedCards: string): any {
+            let cardInfo: any = {
+                "2": {Word: "Two", Occurance: 0},
+                "3": {Word: "Three", Occurance: 0},
+                "4": {Word: "Four", Occurance: 0},
+                "5": {Word: "Five", Occurance: 0},
+                "6": {Word: "Six", Occurance: 0},
+                "7": {Word: "Seven", Occurance: 0},
+                "8": {Word: "Eight", Occurance: 0},
+                "9": {Word: "Nine", Occurance: 0},
+                "T": {Word: "Ten", Occurance: 0},
+                "J": {Word: "Jack", Occurance: 0},
+                "Q": {Word: "Queen", Occurance: 0},
+                "K": {Word: "King", Occurance: 0},
+                "A": {Word: "Ace", Occurance: 0},
 
-            if (sortedCards[i][0] === lastCard[0]) {
-                consecutiveHops++;
+            };
+            for (let i: number = 0; i < sortedCards.length; i++) {
+                cardInfo[sortedCards[i][0]].Occurance += 1;
+                console.log(cardInfo[sortedCards[i][0]].Occurance);
+
             }
-            lastCard = sortedCards[i];
-        }
-        return consecutiveHops;
+            return cardInfo;
     }
 
-    private static findHighestCard(sortedCards: string): string {
-        let cardWords: any = {
-            "A": "Ace",
-            "8": "Eight"
-        }
-
-        let highestCardValue: string = cardWords[sortedCards[0][0]];
+    private static findHighestCard(sortedCards: string, cardInfo: any): string {
+        let highestCardValue: string = cardInfo[sortedCards[0][0]].Word;
+        console.log(highestCardValue + "!!!");
         return "High Card: " + highestCardValue;
     }
 }
+
+
+///OCCURANCE!!! FOR EXAMPLE, "A": {"WORD": "ACE", "OCCURANCE": 1}
