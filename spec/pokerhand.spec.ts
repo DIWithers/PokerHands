3ebf02014d372b67ee454fd610f1bb52f3bd4be9
DIWithers <d.ivy.withers@gmail.com>
2017-11-22 09:40:39 -0500

    added second high card test, refactor to find high cards other than ace

--- spec/pokerhand.spec.ts
+++ spec/pokerhand.spec.ts
@@ -1,8 +1,11 @@
 import {PokerHand} from "../src/pokerhand";
 
 describe("Poker Hands", () => {
-    it("should return highest card value", () => {
-     expect(PokerHand.findRank()).toBe("High Card: Ace");
+    it("should return highest card value if Ace", () => {
+     expect(PokerHand.findRank("2C 3H 4S 8C AH")).toBe("High Card: Ace");
+    });
+    it("should return highest card value if Eight", () => {
+        expect(PokerHand.findRank("2C 3H 4S 8C 4H")).toBe("High Card: Eight");
     });
 });
 
--- src/pokerhand.ts
+++ src/pokerhand.ts
@@ -1,6 +1,15 @@
 export class PokerHand {
 
-    static findRank(): string {
-        return "High Card: Ace";
+    static findRank(cards: string): string {
+        let sortedCards: any = cards.split(" ").sort().reverse();
+        let cardHandInfo: any  = new Map<string, any>();
+        cardHandInfo.set("A", {"Word": "Ace"});
+        cardHandInfo.set("8", {"Word": "Eight"});
+        return PokerHand.findHighestCardValue(sortedCards, cardHandInfo);
+    }
+
+    private static findHighestCardValue(sortedCards: any, cardHandInfo: any): string {
+        let highCard: any = cardHandInfo.get(sortedCards[0][0]);
+        return "High Card: " + highCard.Word;
     }
 }
